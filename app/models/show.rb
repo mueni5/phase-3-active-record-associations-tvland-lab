7d91daabf50f70f2d42d3b7673645a6c386f7105
+ <+>
2023-06-03 16:41:43 +0300

    in progress

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+  
+    belongs_to :network
+  
+    def actors_list
+      self.actors.collect do |char|
+          "#{char.first_name} #{char.last_name}"
+      end.join
+    end
   
 end
