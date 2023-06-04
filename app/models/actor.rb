ee4309dec6de218afef0372b06fad424f784e903
+ <+>
2023-06-04 14:54:35 +0300

    Complete

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,14 +3,14 @@ class Actor < ActiveRecord::Base
     has_many :shows, through: :characters
   
     def full_name
-      "#{first_name} #{last_name}"
+      "#{self.first_name} #{self.last_name}"
     end
   
     def list_roles
-        self.characters.collect do |char|
-            "#{char.name} - #{char.show.name}"
-        end.join
+      self.characters.map do |character|
+        "#{character.name} - #{character.show.name}"
+      end
     end
-end
   
+end
   
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,13 +1,12 @@
 class Show < ActiveRecord::Base
-    has_many :characters
-    has_many :actors, through: :characters
-  
-    belongs_to :network
-  
-    def actors_list
-      self.actors.collect do |char|
-          "#{char.first_name} #{char.last_name}"
-      end.join
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.actors.map do |actor|
+      actor.full_name
     end
-  
+  end
+
 end
